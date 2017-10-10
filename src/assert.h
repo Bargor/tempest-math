cf1523c3e78cf09f990ff3ceb333f27339432802
Karol Kontny <barolek@gmail.com>
2017-10-10 23:35:44 +0200

    Added assert prototype

---
+++ src/assert.h
@@ -0,0 +1,24 @@
+# This file is part of Tempest-math project
+# Author: Karol Kontny
+
+#pragma once
+#incnlude <cstdio>
+
+namespace tst 
+{
+
+    inline void _assert(const char* expression, const char* file, int line)
+    {
+        fprintf(stderr, "Assertion '%s' failed, file '%s' line '%d'.", expression, file, line);
+        abort();
+    }
+
+#undef assert
+
+#ifdef NDEBUG
+#define assert(EXPRESSION) ((void)0)
+#else
+#define assert(EXPRESSION) ((EXPRESSION) ? (void)0 : _assert(#EXPRESSION, __FILE__, __LINE__))
+#endif
+
+}
