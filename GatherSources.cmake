f09793ae006bb1a01fb068f94be64c2690203a1d
Karol Kontny <barolek@gmail.com>
2017-10-18 16:47:08 +0200

    First part of project building code

---
+++ GatherSources.cmake
@@ -0,0 +1,9 @@
+# This file is part of Tempest-math project
+# Author: Karol Kontny
+
+set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/tstmath.h)
+set(TEMPEST_CODE ${CMAKE_SOURCE_DIR}/src/assert.h)
+set(TEMPEST_CODE ${CMAKE_SOURCE_DIR}/src/util.h)
+
+set(TEMPEST_CODE ${TEMPEST_CODE} PARENT_SCOPE)
+
