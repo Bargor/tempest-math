617091e424e503f85e4423b3c44f0bc00e9278ab
Karol Kontny <barolek@gmail.com>
2018-01-16 16:01:17 +0100

    Fixed tests

--- test/unit_test/math_common_test.cpp
+++ test/unit_test/math_common_test.cpp
@@ -18,10 +18,10 @@ namespace tst {
     TEST(MathCommonTest, sqrtFastVector) {
         vec4 v(1.0f, 4.0f, 9.0f, 16.0f);
         v = sqrt_fast(v);
-        EXPECT_EQ(v.x, 1.0f);
-        EXPECT_EQ(v.y, 2.0f);
-        EXPECT_EQ(v.z, 3.0f);
-        EXPECT_EQ(v.w, 4.0f);
+        EXPECT_TRUE(1.0f - v.x < 0.001f );
+        EXPECT_TRUE(2.0f - v.y < 0.001f);
+        EXPECT_TRUE(3.0f - v.z < 0.001f);
+        EXPECT_TRUE(4.0f - v.w < 0.001f);
     }
 
     TEST(MathCommonTest, sqrtScalar) {
@@ -33,7 +33,7 @@ namespace tst {
     TEST(MathCommonTest, sqrtFastScalar) {
         float v = 4.0f;
         v = sqrt_fast(v);
-        EXPECT_EQ(v, 2.0f);
+        EXPECT_TRUE(2.0f - v < 0.001f);
     }
 
     TEST(MathCommonTest, dotVector) {
